4ea91031f41ab2f5a139b411f4025090e73806c5
Shreejit Murthy <botmorph17@gmail.com>
2024-09-09 14:04:08 +0930

    utilising working camera

--- examples/main.c
+++ examples/main.c
@@ -2,18 +2,24 @@
 #include "snap.h"
 
 int main() {
-    gfx_init((snp_window_args){
+    snp_gfx_init((snp_window_args){
         .title = "Window",
         .width = 800,
         .height = 600,
         .clear_colour.hex = 0x6495ED
     });
 
+    snp_camera camera = snp_camera_init();
+    camera.zoom = 1.0f;
+    snp_camera_setpos(&camera, 200, 300);  // offset camera slightly from centre to show effect
+
     snp_texture texture = snp_texture_init("../examples/player_sprite_borderless.png");
     snp_quad quad = (snp_quad){16, 0, 16, 32};
 
-    while (gfx_window_open()) {
-        gfx_clear();
+    while (snp_gfx_window_open()) {
+        snp_gfx_clear();
+
+        snp_camera_attach(camera);
 
         snp_texture_draw((snp_texture_draw_args){
             .texture = texture,
@@ -23,9 +29,13 @@ int main() {
             .sx = 2, .sy = 2
         });
 
-        gfx_refresh();
+        snp_camera_detach(camera);
+
+        snp_gfx_refresh();
     }
 
-    gfx_destroy();
+    snp_texture_delete(texture);
+
+    snp_gfx_destroy();
     return 0;
 }
