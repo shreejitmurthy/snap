268e782d558c17d57f3fa596f011c4e80a8d64b9
Shreejit Murthy <botmorph17@gmail.com>
2024-09-10 12:33:05 +0930

    added more textures

--- examples/main.c
+++ examples/main.c
@@ -3,18 +3,23 @@
 
 int main() {
     snp_gfx_init((snp_window_args){
-        .title = "Window",
-        .width = 800,
-        .height = 600,
-        .clear_colour.hex = 0x6495ED
+            .title = "Window",
+            .width = 800,
+            .height = 600,
+            .clear_colour.hex = 0x334d4d
     });
 
     snp_camera camera = snp_camera_init();
     camera.zoom = 1.0f;
-    snp_camera_setpos(&camera, 200, 300);  // offset camera slightly from centre to show effect
+    snp_camera_setpos(&camera, 400, 300);
 
-    snp_texture texture = snp_texture_init("../examples/player_sprite_borderless.png");
-    snp_quad quad = (snp_quad){16, 0, 16, 32};
+    snp_texture player_spritesheet = snp_texture_init("../examples/player.png");
+    snp_quad player_quad = (snp_quad){0, 0, 64, 64};
+
+    snp_texture knight = snp_texture_init("../examples/knight.png");
+
+    snp_texture animals = snp_texture_init("../examples/animals.png");
+    snp_quad kangaroo_quad = (snp_quad){1, 387, 32, 32};
 
     while (snp_gfx_window_open()) {
         snp_gfx_clear();
@@ -22,19 +27,33 @@ int main() {
         snp_camera_attach(camera);
 
         snp_texture_draw((snp_texture_draw_args){
-            .texture = texture,
-            .quad = quad,
-            .x = 400,
-            .y = 300,
-            .sx = 2, .sy = 2
+                .texture = player_spritesheet,
+                .quad = player_quad,
+                .x = 400,
+                .y = 300,
         });
 
-        snp_camera_detach(camera);
+        snp_texture_draw((snp_texture_draw_args){
+                .texture = knight,
+                .x = 100,
+                .y = 100,
+                .sx = 2, .sy = 2
+        });
+
+        snp_camera_detach();
+
+        snp_texture_draw((snp_texture_draw_args){
+                .texture = animals,
+                .quad = kangaroo_quad,
+                .x = 500,
+                .y = 400,
+                .sx = 2, .sy = 2
+        });
 
         snp_gfx_refresh();
     }
 
-    snp_texture_delete(texture);
+    snp_texture_delete(player_spritesheet);
 
     snp_gfx_destroy();
     return 0;
