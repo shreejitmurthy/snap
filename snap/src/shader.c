48613ca8c8c4ba3c59b6236813c49aeef58f6ae3
Shreejit Murthy <botmorph17@gmail.com>
2024-09-18 10:14:13 +0930

    using readFile from utils.h

--- snap/src/shader.c
+++ snap/src/shader.c
@@ -4,8 +4,8 @@
 
 #include "shader.h"
 #include <glad/glad.h>
-#include <SDL3/SDL_iostream.h>
 #include <log/log.h>
+#include "utils.h"
 
 void checkCompileErrors(unsigned int shader, const char* type) {
     int success;
@@ -25,49 +25,6 @@ void checkCompileErrors(unsigned int shader, const char* type) {
     }
 }
 
-char* readFile(const char* filePath) {
-    if (filePath != NULL) {
-        // Open the file
-        SDL_IOStream* io = SDL_IOFromFile(filePath, "rb");
-        if (io == NULL) {
-            log_error("SNP::Shader file unsuccessfully read: %s", filePath);
-            return NULL;
-        }
-
-        // Get the length of the file
-        Sint64 length = SDL_GetIOSize(io);
-        if (length < 0) {
-            log_error("SNP::SDL_IOStream file size error");
-            SDL_CloseIO(io);
-            return NULL;
-        }
-
-        // Allocate memory to read the file content
-        char* content = (char*)malloc(length + 1);
-        if (content == NULL) {
-            log_error("SNP::Shader memory allocation failed");
-            SDL_CloseIO(io);
-            return NULL;
-        }
-
-        // Read the file content
-        Sint64 bytesRead = SDL_ReadIO(io, content, length);
-        if (bytesRead != length) {
-            log_error("SNP::SDL_IOStream file read error");
-            free(content);
-            SDL_CloseIO(io);
-            return NULL;
-        }
-
-        // Null-terminate the content
-        content[length] = '\0';
-
-        SDL_CloseIO(io);
-        return content;
-    } else {
-        return NULL;
-    }
-}
 
 snp_shader snp_shader_init(snp_shader_args args) {
     snp_shader shader;
